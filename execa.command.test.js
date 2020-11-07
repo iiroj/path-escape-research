76231d793b605bd078ce98584206be25049989d1
Iiro Jäppinen <iiro@jappinen.fi>
2020-11-07 09:21:44 +0200

    test: run only tests that shuld pass

--- execa.command.test.js
+++ execa.command.test.js
@@ -1,75 +1,11 @@
 const execa = require('execa')
 
-// prettier-ignore
-const ARGS = ['test/file.js', 'test/{file.js}', 'test/file with spaces.js']
-// prettier-ignore
-const ARGS_SINGLE_QUOTES = ["'test/file.js'", "'test/{file.js}'", "'test/file with spaces.js'"]
-// prettier-ignore
-const ARGS_DOUBLE_QUOTES = ['"test/file.js"', '"test/{file.js}"', '"test/file with spaces.js"']
-// prettier-ignore
-const ARGS_ESCAPED = ['test\/file.js', 'test\/\{file.js\}', 'test\/file\ with\ spaces.js']
-// prettier-ignore
-const ARGS_ESCAPED_DOUBLE_SPACES = ['test\/file.js', 'test\/\{file.js\}', 'test\/file\\ with\\ spaces.js']
+const ARGS = ['test/file.js', 'test/{file.js}', 'test/file\\ with\\ spaces.js']
 
 describe('execa.command', () => {
   const cmd = (args) => execa.command(`prettier --check ${args.join(' ')}`, { all: true })
 
-  it('should fail with no escaping', async () => {
-    await expect(cmd(ARGS)).rejects.toThrowErrorMatchingInlineSnapshot(`
-            "Command failed with exit code 2: prettier --check test/file.js test/{file.js} test/file with spaces.js
-            [error] No files matching the pattern were found: \\"test/{file.js}\\".
-            [error] No files matching the pattern were found: \\"test/file\\".
-            [error] No files matching the pattern were found: \\"with\\".
-            [error] No files matching the pattern were found: \\"spaces.js\\".
-            Checking formatting...
-            All matched files use Prettier code style!"
-          `)
-  })
-
-  it('should fail with single quotes', async () => {
-    await expect(cmd(ARGS_SINGLE_QUOTES)).rejects.toThrowErrorMatchingInlineSnapshot(`
-            "Command failed with exit code 2: prettier --check 'test/file.js' 'test/{file.js}' 'test/file with spaces.js'
-            [error] No files matching the pattern were found: \\"'test/file.js'\\".
-            [error] No files matching the pattern were found: \\"'test/{file.js}'\\".
-            [error] No files matching the pattern were found: \\"'test/file\\".
-            [error] No files matching the pattern were found: \\"with\\".
-            [error] No files matching the pattern were found: \\"spaces.js'\\".
-            Checking formatting...
-            All matched files use Prettier code style!"
-          `)
-  })
-
-  it('should fail with double quotes', async () => {
-    await expect(cmd(ARGS_DOUBLE_QUOTES)).rejects.toThrowErrorMatchingInlineSnapshot(`
-            "Command failed with exit code 2: prettier --check \\"test/file.js\\" \\"test/{file.js}\\" \\"test/file with spaces.js\\"
-            [error] No files matching the pattern were found: \\"\\"test/file.js\\"\\".
-            [error] No files matching the pattern were found: \\"\\"test/{file.js}\\"\\".
-            [error] No files matching the pattern were found: \\"\\"test/file\\".
-            [error] No files matching the pattern were found: \\"with\\".
-            [error] No files matching the pattern were found: \\"spaces.js\\"\\".
-            Checking formatting...
-            All matched files use Prettier code style!"
-          `)
-  })
-
-  it('should fail with escaped characters', async () => {
-    await expect(cmd(ARGS_ESCAPED)).rejects.toThrowErrorMatchingInlineSnapshot(`
-            "Command failed with exit code 2: prettier --check test/file.js test/{file.js} test/file with spaces.js
-            [error] No files matching the pattern were found: \\"test/{file.js}\\".
-            [error] No files matching the pattern were found: \\"test/file\\".
-            [error] No files matching the pattern were found: \\"with\\".
-            [error] No files matching the pattern were found: \\"spaces.js\\".
-            Checking formatting...
-            All matched files use Prettier code style!"
-          `)
-  })
-
-  it('should fail with escaped characters (double spaces)', async () => {
-    await expect(cmd(ARGS_ESCAPED_DOUBLE_SPACES)).rejects.toThrowErrorMatchingInlineSnapshot(`
-            "Command failed with exit code 2: prettier --check test/file.js test/{file.js} test/file with spaces.js
-            [error] No files matching the pattern were found: \\"test/{file.js}\\".
-            Checking formatting...
-            All matched files use Prettier code style!"
-          `)
+  it('should run prettier', async () => {
+    await expect(cmd(ARGS)).resolves.toMatchInlineSnapshot()
   })
 })
